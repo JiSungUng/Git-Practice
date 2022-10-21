106034ea3cdafad308222bb4a19e9a405e5d2412
JiSungUng <dnfl3455@naver.com>
2022-10-21 23:29:51 +0900

    func||docs : 첫 기능(PC랜덤숫자배정) 모듈화

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-// 기능 및 게임 시작 분리!
-// function printNum() {
-//   const userInput = document.getElementById('user-input').value;
-//   document.getElementById('result').innerText = userInput;
-// }
-
-// function makePCNum() {
-//   const randNum = MissionUtils.Random.pickNumberInRange(1, 9);
-//   document.getElementById('pc-result').innerText = randNum
-// }
+import getComputerInput from './src/input/getComputerInput.js'
 
 export default function BaseballGame() {
   const computerInputNumbers = getComputerInput();
 
-  this.play = function(computerInputNumbers, userInputNumbers) {
-    return "결과 값 String"
-  }
-
-  
-
+  console.log(computerInputNumbers);
+  // this.play = function(computerInputNumbers, userInputNumbers) {
+  //   return "결과 값 String"
+  // }
 }
+
+new BaseballGame();
 
 
